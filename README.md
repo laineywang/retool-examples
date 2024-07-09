74e44c4997ee82bf805ae46ef9fc06ee5a00c6df
merunga <merunga@gmail.com>
2024-07-08 20:16:33 -0500

    replication steps

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# retool-examples
+# Resources for retool community post
+
+- First copy the `.env.sample` file to `.env` and fill in the values.
+- install deps with npm and [install playwright binaries and deps](https://playwright.dev/docs/intro)
+- import de apps in the `retool-apps` folder into your retool account
+- get the url for the apps and put any of them inside the `.env` file as `RETOOL_APP_URL`
+- run the `test` script: `npm run test`
---
+++ tests/example.spec.js
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+import dotenv from "dotenv";
+dotenv.config();
+
+test("example test", async ({ page }) => {
+  await page.goto(process.env.RETOOL_APP_URL);
+  await page
+    .getByPlaceholder("name@company.com")
+    .fill(`${process.env.PLAYWRIGHT_USERNAME}`);
+  await page
+    .getByPlaceholder("Password")
+    .fill(`${process.env.PLAYWRIGHT_PASSWORD}`);
+  await page.getByText(/^Sign in$/).click();
+
+  await expect(page.getByText("Form title")).toBeVisible();
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Ok" }).click();
+  await page.getByText("Lawton, Oklahoma, United States").click();
+});
